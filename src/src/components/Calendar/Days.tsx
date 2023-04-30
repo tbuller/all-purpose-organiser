6498aad34651dc89b5a882a3a2e9c7a5aac02f6d
Tim Buller <111061411+tbuller@users.noreply.github.com>
2023-04-30 16:05:43 +0200

    events successfully rendered in month view in a smaller version

--- src/src/components/Calendar/Days.tsx
+++ src/src/components/Calendar/Days.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { RootStateUsers } from '../../redux/usersSlice';
 import { RootStateCalendar } from '../../redux/calendarSlice';
 import { setSelectedDay } from '../../redux/calendarSlice';
+import { setLoggedInUser } from '../../redux/usersSlice';
 import '../../styling/Calendar/Days.scss'
 
 const Days = () => {
 
   const dispatch = useDispatch();
+  const loggedInUser = useSelector((state: RootStateUsers) => state.users.loggedInUser);
   const daysOfMonth = useSelector((state: RootStateCalendar) => state.calendar.daysOfMonth);
   const todaysDate = useSelector((state: RootStateCalendar) => state.calendar.todaysDate);
+  const events = useSelector((state: RootStateCalendar) => state.calendar.events);
 
   const handleDaySelect = (day: string) => {
     dispatch(setSelectedDay(day));
@@ -23,6 +27,15 @@ const Days = () => {
           <p className={`day-number${todaysDate === day ? " selected" : ""}`}>{day.substring(9, 11)[1] === "," ? day.substring(9, 10) : day.substring(9, 11)}</p>
           <p className="day-of-the-week">{day.substring(0, 3)}</p>
           </span>
+          <div className="day-component-events-container">
+            {events.filter(event => event.day === day && event.creatorId === loggedInUser?._id).map(event => 
+              <span className="day-component-individual-event-container" key={event._id}>
+                <p className="day-component-event-title">{event.title}</p>
+                <p className="day-component-event-time">{event.time}</p>
+                <button className="day-component-event-button">X</button>
+              </span>  
+              )}
+          </div>
         </div>
         )}
     </div>
