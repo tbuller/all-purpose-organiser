31650e3d1d807c11f02379d68d262b3571252e8b
Tim Buller <111061411+tbuller@users.noreply.github.com>
2023-05-01 18:59:30 +0200

    new route set up for the patch request

--- api/controllers/shoppingItems.js
+++ api/controllers/shoppingItems.js
@@ -19,6 +19,16 @@ const ShoppingItemsController = {
         res.status(200).json({ message: "OK", shoppingItems: shoppingItems });
       }
     })
+  },
+  ChangeQuantity: (state, action) => {
+    const { itemId, quantityChange } = req.body;
+    ShoppingItem.findByIdAndUpdate(itemId, { $inc: { quantity: quantityChange } }, { new: true }, (err, shoppingItem) => {
+      if (err) {
+        res.status(500).json({ message: "Bad request", err: err })
+      } else {
+        res.status(200).json({ message: "OK", shoppingItem: shoppingItem });
+      }
+    })
   }
 }
 
--- api/routes/shoppingItems.js
+++ api/routes/shoppingItems.js
@@ -5,5 +5,6 @@ const ShoppingItemsController = require("../controllers/shoppingItems");
 
 router.post("/", ShoppingItemsController.Create);
 router.get("/", ShoppingItemsController.List);
+router.patch("/", ShoppingItemsController.ChangeQuantity);
 
 module.exports = router;
