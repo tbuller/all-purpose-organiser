b666cd28ae627caa5c03575ab850954ff50fa05a
Tim Buller <111061411+tbuller@users.noreply.github.com>
2023-05-01 12:29:16 +0200

    redux implemented to dynamically delete note

--- src/src/components/Notes/NoteHeader.tsx
+++ src/src/components/Notes/NoteHeader.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Note } from '../../redux/notesSlice';
-import { setSelectedNote } from '../../redux/notesSlice';
+import { setSelectedNote, removeNote } from '../../redux/notesSlice';
 import '../../styling/Notes/NoteHeader.scss';
 
 interface NoteHeaderProps {
@@ -17,8 +17,24 @@ const NoteHeader: React.FC<NoteHeaderProps> = ({ note }) => {
     dispatch(setSelectedNote(note));
   }
 
+  const handleRemoveNote = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    fetch("http://localhost:8080/notes", {
+      method: "delete",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ _id: note._id })
+    })
+      .then(response => response.json())
+      .then(data => {
+        dispatch(removeNote(data.note));
+      })
+  }
+
   return (
     <div className="note-header-container" onClick={handleSetSelectedNote}>
+      <button className="note-header-delete-button" onClick={(e) => handleRemoveNote(e)}>X</button>
       <p className="note-header-title">{note.title}</p>
     </div>
   )
