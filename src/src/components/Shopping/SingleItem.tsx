42941303f6fbb3c156f0eb1a7ac71a1a5d056b1d
Tim Buller <111061411+tbuller@users.noreply.github.com>
2023-05-09 10:34:51 +0200

    some styling for shopping item

--- src/src/components/Shopping/SingleItem.tsx
+++ src/src/components/Shopping/SingleItem.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Item } from '../../redux/shoppingSlice';
 import { updateItem } from '../../redux/shoppingSlice';
+import '../../styling/Shopping/SingleItem.scss';
 
 interface SingleItemProps {
   item: Item;
@@ -42,10 +43,10 @@ const SingleItem: React.FC<SingleItemProps> = ({ item }) => {
 
   return (
     <span className="individual-shopping-item-container">
-      <img src={itemPhotoUrl} />
-      <p>{item.name}</p>
-      <p>{item.quantity}</p>
-      <button className="change-quantity-button" onClick={() => handleChangeQuantity(1)}>+</button>
+      <img className="shopping-item-photo" src={itemPhotoUrl} />
+      <p className="shopping-item-name">{item.name}</p>
+      <p className="shopping-item-quantity">{item.quantity}</p>
+      <button className="change-quantity-button add" onClick={() => handleChangeQuantity(1)}>+</button>
       <button className="change-quantity-button" onClick={() => handleChangeQuantity(-1)}>-</button>
     </span>
   )
