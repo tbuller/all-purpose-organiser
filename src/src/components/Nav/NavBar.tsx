f1ce95390cf01527628ca6c3ccb16cf4e7e167d2
Tim Buller <111061411+tbuller@users.noreply.github.com>
2023-05-15 18:08:18 +0200

    more final touches

--- src/src/components/Nav/NavBar.tsx
+++ src/src/components/Nav/NavBar.tsx
@@ -3,14 +3,16 @@ import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootStateNav } from '../../redux/navSlice';
 import { setViewedComponent } from '../../redux/navSlice';
+import { useNavigate } from 'react-router-dom';
 import { BsFillCalendarFill } from 'react-icons/bs';
 import { GiShoppingCart } from 'react-icons/gi';
 import { GrNotes } from 'react-icons/gr'
-import { FaUserFriends } from 'react-icons/fa';
+import { ImExit } from 'react-icons/im';
 import '../../styling/Nav/NavBar.scss';
 
 const NavBar = () => {
 
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const viewedComponent = useSelector((state: RootStateNav) => state.nav.viewedComponent);
 
@@ -23,6 +25,7 @@ const NavBar = () => {
       <button className={`nav-bar-button${viewedComponent === "calendar" ? " selected" : ""}`} onClick={() => handleSetComponent("calendar")}><BsFillCalendarFill className="calendar-icon" /></button>
       <button className={`nav-bar-button${viewedComponent === "shopping" ? " selected" : ""}`} onClick={() => handleSetComponent("shopping")}><GiShoppingCart className="shopping-icon" /></button>
       <button className={`nav-bar-button${viewedComponent === "notes" ? " selected" : ""}`} onClick={() => handleSetComponent("notes")}><GrNotes className="notes-icon" /></button>
+      <button className="nav-bar-button" onClick={() => navigate("/")}><ImExit className="exit-icon" /></button>
     </div>
   )
 }
